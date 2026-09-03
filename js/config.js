a224a9963665d075193280db3168979f708c9865
lucradison2-bit <lucradison2@gmail.com>
2026-09-03 23:22:12 +0300

    Add configuration constants for Supabase and loans

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const SUPABASE_URL = 'https://poly.supabase.co'
+export const SUPABASE_KEY = 'poly123poly123'
+export const SITE_URL = window.location.origin
+
+export const LOAN_DAYS = 14
+export const DAILY_PENALTY = 500
+export const GRACE_DAYS = 2
