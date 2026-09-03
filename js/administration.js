377095dfe83d71ba56948377fba626f464db8a00
lucradison2-bit <lucradison2@gmail.com>
2026-09-03 23:37:52 +0300

    Update administration.js

--- js/administration.js
+++ js/administration.js
@@ -1,1 +1,15 @@
-
+import { supabase } from './supabase.js'
+import {
+  bookStatusLabel,
+  escapeHtml,
+  formatDate,
+  formatMoney,
+  loanStatusLabel,
+  renderLayout,
+  requireAdmin,
+  showMessage,
+} from './commun.js'
+import {
+  DEFAULT_DAILY_RATE,
+  DEFAULT_GRACE_DAYS,
+} from './config.js'
